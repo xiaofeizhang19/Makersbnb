c715c4f449563cb490f44a572ed82594099ea890
Sayem995 <sayem.bokhari.1@gmail.com>
2019-09-23 15:04:18 +0100

    add test and code for constructor

--- spec/spaceSpec.js
+++ spec/spaceSpec.js
@@ -4,11 +4,34 @@ describe('Space', function() {
   var space;
 
   beforeEach(function() {
-    space = new Space('House 1');
+    space = new Space('House 1', '2 Bed Flat', 'Mr. Smith', 'Bond Street', 200, 4);
   });
 
   it('saves name of a space', function() {
     expect(space.name).toEqual('House 1');
   })
+
+  it('saves property type', function() {
+    expect(space.type).toEqual('2 Bed Flat');
+  })
+
+  it('shows owner name', function() {
+    expect(space.owner).toEqual('Mr. Smith');
+  })
+
+  it('shows the location', function() {
+    expect(space.location).toEqual('Bond Street');
+  })
+
+  it('shows the price', function() {
+    expect(space.price).toEqual(200);
+  })
+
+  it('shows the capacity', function() {
+    expect(space.capacity).toEqual(4);
+  })
+
+
+
 
 });
--- src/space.js
+++ src/space.js
@@ -1,3 +1,8 @@
-function Space(name) {
+function Space(name, type, owner, location, price, capacity) {
   this.name = name;
+  this.type = type;
+  this.owner = owner;
+  this.location = location;
+  this.price = price;
+  this.capacity = capacity;
 };
