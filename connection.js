99f4bf9d57490e0365d8eabbaa7e73695e23b3de
Valeria Serci <balerty84@gmail.com>
2019-09-25 21:59:58 +0100

    Add query function to help queries execution

--- connection.js
+++ connection.js
@@ -1,23 +1,18 @@
 const { Client } = require('pg')
-const client = new Client({
-    user: 'makersbnb_user',
-    host: 'localhost',
-    database: 'makersbnb',
-    password: '',
-    port: 5432,
-  })
-client.connect()
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-});
+// I create a function to help queries execution
+function query(query, parameters, callback) {
+    const client = new Client({
+        user: 'makersbnb_user',
+        host: 'localhost',
+        database: 'makersbnb',
+        password: '',
+        port: 5432,
+      });
+    client.connect();
+    client.query(query, parameters, (err, res) => {
+        client.end();
+        callback(err, res);
+    });    
+};
 
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end();
-});
-
-//   client.query('INSERT into owner (username, password_hash, email) VALUES ($1::text, $2::text, $3::text)', ['cat', '12345', 'cat@gmail.com'], (err, res) => {
-//     console.log(err ? err.stack : res)
-//     client.end()
-
-// });
+module.exports = query;
