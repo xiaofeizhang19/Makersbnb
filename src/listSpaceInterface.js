07d40e31fe01098fed87ac457db7a9a128f3f3aa
James Holton <james_holton@yahoo.co.uk>
2019-09-25 11:21:44 +0100

    Made a calander appear... yay

--- src/listSpaceInterface.js
+++ src/listSpaceInterface.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
-// space = new Space("Space1", "2 bed flat", 200, 5);
-$('#mainmenu').click(function(){
-  window.location.replace("./menu.html");
-});
-var name = sessionStorage.getItem('name', name);
-var description = sessionStorage.getItem('description', description);
-var capacity = sessionStorage.getItem('capacity', capacity);
-var price = sessionStorage.getItem('price', price);
-var dateFrom = sessionStorage.getItem('dateFrom', dateFrom);
-var dateTo = sessionStorage.getItem('dateTo', dateTo);
+  // space = new Space("Space1", "2 bed flat", 200, 5);
+  $('#mainmenu').click(function(){
+    window.location.replace("./menu.html");
+  });
 
-$('#name').html(name);
-$('#capacity').html(capacity);
-$('#price').html(price);
-$('#description').html(description);
+  var name = sessionStorage.getItem('name', name);
+  var description = sessionStorage.getItem('description', description);
+  var capacity = sessionStorage.getItem('capacity', capacity);
+  var price = sessionStorage.getItem('price', price);
+  var dateFrom = sessionStorage.getItem('dateFrom', dateFrom);
+  var dateTo = sessionStorage.getItem('dateTo', dateTo);
+
+  $('#name').html(name);
+  $('#capacity').html(capacity);
+  $('#price').html(price);
+  $('#description').html(description);
+
+  pickmeup('input', {
+    hide_on_select:false,
+    mode:'range',
+    render:function () {
+      return {disabled : true, class_name}
+    }
+  });
 });
