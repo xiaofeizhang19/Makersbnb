94ac97ad590f7167cf07b07a6d5d8409c530d239
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-26 18:20:22 +0100

    Working on login

--- data/users.js
+++ data/users.js
@@ -1,15 +1,31 @@
 const query = require('./connection');
 module.exports = {
-    create: (data, response) => {
-        console.log('hello we are in data');
-        console.log(data);
-
+  create: (data, response) => {
     query('INSERT INTO users (username, password_hash, email) VALUES ($1::text, $2::text, $3::text)', 
-        [data.username, data.password, data.email], (err, res) => {
-            console.log('callback');
-            console.log('wuauuahkbkhvbhsjvshjg');
-           });
-    }
+      [data.username, data.password, data.email], (err, res) => {
+        console.log('hello from signing up');
+      });
+  },
+
+  login: (data, response) => {
+    query('SELECT * FROM users WHERE username = $1::text', 
+      [data.username], (err, result, fields) => {
+        console.log(result);
+        if (err) {
+          console.log("error");
+          return;
+        }
+        // var rows = JSON.parse(JSON.stringify(results[0]));
+        // console.log(rows);
+        // if (result.length > 0) {
+        //   console.log("fine");
+        // } 
+        // if (row.length === 0) {
+        //   console.log('user does not exist')
+        // }
+      });
+      
+  }
 }
 // module.exports = {
 //   create: (data, response) => {
--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 /* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  console.log(req.params.id, "Aaaaaaaaaaaaaa")
-  res.send('Get');
-});
+// router.get('/:id', function(req, res, next) {
+//   console.log(req.params.id, "Aaaaaaaaaaaaaa")
+//   res.send('Get');
+// });
 
 /* CREATE user */
 router.post('/', function(req, res) {
-  console.log('router');
-  console.log(req.body)
   users.create(req.body, res);
-  res.send("hello")
+  res.send("You have signed up!")
+});
+
+router.post('/login', function(req, res) {
+  console.log(req.body)
+  users.login(req.body, res);
+  res.send("logged in!")
 });
 
 module.exports = router;
