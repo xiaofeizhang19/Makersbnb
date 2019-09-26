b25691006d1a74829191b5f87746570ffebeba0e
James Holton <james_holton@yahoo.co.uk>
2019-09-26 14:17:11 +0100

    naughty master update she made me do it again

--- data/users.js
+++ data/users.js
@@ -1,13 +1,13 @@
-const query = require('connection');
+// const query = require('connection');
 
-module.exports = {
-  create: (data, response) => {
-    console.log('create');
-    query(
-      // 'INSERT ...',
-      // [data.username, data.password, data.email],
-      // (err, result) => {
-      //   console.log('callback');
-      //   response.send(result);
-  // }
-// }
+// module.exports = {
+//   create: (data, response) => {
+//     console.log('create');
+//     query(
+//       // 'INSERT ...',
+//       // [data.username, data.password, data.email],
+//       // (err, result) => {
+//       //   console.log('callback');
+//       //   response.send(result);
+//   // }
+// // }
