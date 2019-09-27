10fb3d65aaafdab02532f11395cd61b9629dcf35
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-27 10:41:32 +0100

    Refactor sign up function. Get rid of callback

--- data/users.js
+++ data/users.js
@@ -4,47 +4,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 module.exports = {
-  create: (data, response) => {
-    bcrypt.hash(
-      data.password, 
-      saltRounds,
-      function(err, hash) {
-        if (err) {
-          console.log(err.stack);
-          response.status(500);
-          response.send({"result": {"error": "NO_CLUE"}});
-          return;
-        }
-
-        query(
-          `INSERT INTO users (username, password_hash, email) 
-          VALUES ($1::text, $2::text, $3::text)
-          RETURNING id`,
-          [data.username, hash, data.email],
-          (err, result) => {
-            if (!err) {
-              const user_id = result.rows[0];
-              response.send({"result": {"user_id": user_id}});
-              return;
-            }
-    
-            if (err.code === "23505") {
-              response.status(409);
-              response.send({"result": {"error": "USER_EXISTS"}});
-            } else if (err.code === "23502") {
-              response.status(400);
-              response.send({"result": {"error": "MISSING_DATA"}});
-            } else {
-              console.log(err.stack);
-              response.status(500);
-              response.send({"result": {"error": "NO_CLUE"}});
-            }
-          }
-        );
-      }
-    );  
-  },
-
+  
   createAsync: async (data, response) => {
     let hash;
     try {
